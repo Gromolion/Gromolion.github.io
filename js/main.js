708656e80d7d5b6e38fd6364302f1f1a0cfd5a4d
Yurii <nais30055003@gmail.com>
2022-06-18 01:40:54 +0300

    shop_and_feedback

--- js/main.js
+++ js/main.js
@@ -1,5 +1,3 @@
-let links = document.getElementsByClassName("header_link");
-
 function openMenu() {
     document.getElementById("background").style.display = "block";
     document.body.style.overflow = "hidden";
@@ -8,13 +6,3 @@ function closeMenu() {
     document.getElementById("background").style.display = "none";
     document.body.style.overflow = "";
 }
-
-setInterval(function () {
-    if (window.pageYOffset >= 100) {
-        document.documentElement.style.setProperty('--header_link-color', "#646464")
-        document.getElementById("header").style.background = "white";
-    } else {
-        document.getElementById("header").style.background = "";
-        document.documentElement.style.setProperty('--header_link-color', "#fff")
-    }
-}, 1000);
