8ac578878012cb83b2a39785fd2538850cfd67d3
Yurii <nais30055003@gmail.com>
2022-06-17 14:08:50 +0300

    mobile_menu

--- js/main.js
+++ js/main.js
@@ -1,5 +1,14 @@
 let links = document.getElementsByClassName("header_link");
 
+function openMenu() {
+    document.getElementById("background").style.display = "block";
+    document.body.style.overflow = "hidden";
+}
+function closeMenu() {
+    document.getElementById("background").style.display = "none";
+    document.body.style.overflow = "";
+}
+
 setInterval(function () {
     if (window.pageYOffset >= 100) {
         document.documentElement.style.setProperty('--header_link-color', "#646464")
